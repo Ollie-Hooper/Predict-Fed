299c1f3af0ae71097826c3dd17f634bf43f55c43
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2023-02-20 15:44:55 +0000

    Added y column as a parameter

--- predict_fed/pipeline.py
+++ predict_fed/pipeline.py
@@ -2,11 +2,12 @@ from sklearn.model_selection import train_test_split
 
 
 class Pipeline:
-    def __init__(self, data_sources, model, evaluator):
+    def __init__(self, data_sources, model, evaluator, y_col='rate'):
         self.data_sources = data_sources
         self.model = model
         self.evaluator = evaluator
         self.features = []
+        self.y_col = y_col
 
     def run(self):
         data = pd.DataFrame()
@@ -17,7 +18,7 @@ class Pipeline:
         self.model.train(X_train, y_train)
 
     def split_data(self, data):
-        y = data['rate']
+        y = data[self.y_col]
         X = data[self.features]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
         return X_train, X_test, y_train, y_test
