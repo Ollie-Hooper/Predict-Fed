4585fb756610c96ec3674785d6e28a8179b088ea
KeatanGill <xe20898@bristol.ac.uk>
2023-02-22 12:14:33 +0000

    updated ann model

--- predict_fed/models/neural_network.py
+++ predict_fed/models/neural_network.py
@@ -1,4 +1,7 @@
-from predict_fed.models.base import Model
+from predict_fed.models.base import Model 
+from keras.models import Sequential
+from keras.layers import Dense
+
 
 
 class NeuralNetwork(Model):
@@ -6,9 +9,35 @@ class NeuralNetwork(Model):
         super().__init__('Neural Network')
 
     def train(self, train_x, train_y):
-        self.trained = True
+       #assuming data is already preprocessed and normalized
+       
+        # Train the model
+        self.trained = True 
+        num_features = train_x.shape[1]
+        Model = Sequential()
+        # Add the input layer
+        Model.add(Dense(12, input_dim=num_features, activation='relu'))
+        #add hidden layer
+        Model.add(Dense(8, activation='relu'))
+        #add output regression layer
+        Model.add(Dense(1, activation='relu')) 
+
+        # Compile the model
+        Model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy']) 
+        # Fit the model
+        Model.fit(train_x, train_y, batch_size=5, epochs=150)   #batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
+        #both hyperparameters can be tuned to improve the model
+        
+        return Model
+
+
+
+
 
     def predict(self, test_x):
         if not self.trained:
             raise Exception(f"Model '{self.name}' has not been trained...")
-        return
+        
+        pred  = self.Model.predict(test_x)
+        return pred  
+    
