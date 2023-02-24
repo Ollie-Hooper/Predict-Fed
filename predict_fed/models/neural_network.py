86c2eefa9e7d60bd38201b575722d1f7b05caae8
KeatanGill <xe20898@bristol.ac.uk>
2023-02-24 09:32:24 +0000

    parameterised epochs and added predicted vs truepl

--- predict_fed/models/neural_network.py
+++ predict_fed/models/neural_network.py
@@ -5,9 +5,10 @@ from keras.layers import Dense
 
 
 class NeuralNetwork(Model):
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, epochs):
         super().__init__('Neural Network')
         self.batch_size = batch_size
+        self.epochs = epochs
 
     def train(self, train_x, train_y):
        #assuming data is already preprocessed and normalized
@@ -26,7 +27,7 @@ class NeuralNetwork(Model):
         # Compile the model
         self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy']) 
         # Fit the model
-        self.model.fit(train_x, train_y, batch_size=self.batch_size, epochs=150)   #batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
+        self.model.fit(train_x, train_y, batch_size=self.batch_size, epochs=self.epochs)   #batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
         #both hyperparameters can be tuned to improve the model
 
 
@@ -48,5 +49,10 @@ class NeuralNetwork(Model):
         pred  = self.model.predict(test_x)
         return pred   
     
+    def plot(test_y, pred):
+        plt.scatter(test_y, pred)
+        plt.xlabel("True Values")
+        plt.ylabel("Predictions")
+        plt.show(
 
     
