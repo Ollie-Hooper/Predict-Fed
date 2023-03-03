7ee4ddd29d79aab92537e80cf73db85b35c81c66
KeatanGill <xe20898@bristol.ac.uk>
2023-03-03 15:14:34 +0000

    modified neural network

--- predict_fed/models/neural_network.py
+++ predict_fed/models/neural_network.py
@@ -2,6 +2,7 @@ from predict_fed.models.base import Model
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -19,14 +20,14 @@ class NeuralNetwork(Model):
         num_features = train_x.shape[1]
         self.model = Sequential()
         # Add the input layer
-        self.model.add(Dense(12, input_dim=num_features, activation='relu'))
+        self.model.add(Dense(64, input_dim=num_features, activation='relu'))
         #add hidden layer
-        self.model.add(Dense(8, activation='relu'))
+        self.model.add(Dense(12, activation='relu'))
         #add output regression layer
-        self.model.add(Dense(1, activation='relu')) 
+        self.model.add(Dense(1, activation='linear')) 
 
         # Compile the model
-        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy']) 
+        self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy']) 
         # Fit the model
         self.model.fit(train_x, train_y, batch_size=self.batch_size, epochs=self.epochs)   #batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
         #both hyperparameters can be tuned to improve the model
@@ -48,12 +49,13 @@ class NeuralNetwork(Model):
             raise Exception(f"Model '{self.name}' has not been trained...")
         
         pred  = self.model.predict(test_x)
-        return pred   
+        rounded_pred = np.round(pred* 4) / 4 
+        return rounded_pred   
     
-    def plot(test_y, pred):
-        plt.scatter(test_y, pred)
-        plt.xlabel("True Values")
-        plt.ylabel("Predictions")
-        plt.show() 
+     
+
+    
+    
+        
 
     
