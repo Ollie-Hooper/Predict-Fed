21c2c48521e92da0f8ecef5f867ec2c4d5863359
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2023-03-08 09:35:39 +0000

    Fixed neural network pipeline

--- predict_fed/models/neural_network.py
+++ predict_fed/models/neural_network.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
 class NeuralNetwork(Model):
     def __init__(self, batch_size, epochs):
         super().__init__('Neural Network')
@@ -13,33 +12,33 @@ class NeuralNetwork(Model):
         self.epochs = epochs
 
     def train(self, train_x, train_y):
-       #assuming data is already preprocessed and normalized
-       
+        # assuming data is already preprocessed and normalized
+
         # Train the model
-        self.trained = True 
+        self.trained = True
         num_features = train_x.shape[1]
         self.model = Sequential()
         # Add the input layer
         self.model.add(Dense(64, input_dim=num_features, activation='relu'))
-        #add hidden layer
+        # add hidden layer
         self.model.add(Dense(12, activation='relu'))
-        #add output regression layer
-        self.model.add(Dense(1, activation='linear')) 
+        # add output regression layer
+        self.model.add(Dense(1, activation='linear'))
 
         # Compile the model
-        self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy']) 
+        self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
         # Fit the model
-        self.model.fit(train_x, train_y, batch_size=self.batch_size, epochs=self.epochs)   #batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
-        #both hyperparameters can be tuned to improve the model
+        self.model.fit(train_x, train_y, batch_size=self.batch_size,
+                       epochs=self.epochs)  # batch_size is the number of samples per gradient update for training and epochs is the number of epochs to train the model
 
+    # both hyperparameters can be tuned to improve the model
 
-
-    def evaluate(self, test_x, test_y):
+    def evaluate(self, train_x, train_y, test_x, test_y):
         if not self.trained:
             raise Exception(f"Model '{self.name}' has not been trained...")
-        
+
         # Evaluate the model
-        scores = self.Model.evaluate(test_x, test_y)
+        scores = self.model.evaluate(test_x, test_y)
         print(f"{self.name} Loss: {scores[0]}")
         print(f"{self.name} Accuracy: {scores[1]}")
         return scores[0], scores[1]
@@ -47,15 +46,7 @@ class NeuralNetwork(Model):
     def predict(self, test_x):
         if not self.trained:
             raise Exception(f"Model '{self.name}' has not been trained...")
-        
-        pred  = self.model.predict(test_x)
-        rounded_pred = np.round(pred* 4) / 4 
-        return rounded_pred   
-    
-     
 
-    
-    
-        
-
-    
+        pred = self.model.predict(test_x)
+        rounded_pred = np.round(pred * 4) / 4
+        return rounded_pred
